0ea82db4af58041bdf5a477cbc436d7af58a8157
Ingrid Oliveira <ingridoliveira@outlook.com>
2023-01-19 16:13:47 -0300

    H2 database, test profile, JPA

--- demo/src/main/java/com/coursespringboot/demo/entities/User.java
+++ demo/src/main/java/com/coursespringboot/demo/entities/User.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
